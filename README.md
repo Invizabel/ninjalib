dc0f89d6818db1dda995b929e36592700a769344
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-06-07 02:36:39 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ﻿# ninjalib is a data science library
 # 
 # import ninjalib
-# cluster = ninjalib.ninjalib(data).cluster()
 # combo = ninjalib.ninjalib(data).combo()
+# cluster = ninjalib.ninjalib(data).diff()
 # flatten = ninjalib.ninjalib(data,nth=0).flatten()
 # mean = ninjalib.ninjalib(data).mean()
 #
 # NOTES:
-# cluster: list expected
 # combo: list expected
+# diff: list expected
 # flatten: list or tuple expected; flatten nth times
 # mean = list or tuple expected
--- src/ninjalib.py
+++ src/ninjalib.py
@@ -1,8 +1,4 @@
-import binascii
 import itertools
-import math
-import statistics
-import struct
 
 class ninjalib:
     def __init__(self,data,a=0,b=0,c=0):
@@ -11,32 +7,23 @@ class ninjalib:
         self.b = b
         self.c = c
 
-    def cluster(self):
-        combos = ninjalib(self.data).combo()
-        distances = []
-        for i in combos:
-            distances.append((abs(i[0]-i[1]),i))
-        distances.sort()
-        j = None
-        clusters = []
-        temp_clusters = []
-        for i in distances:
-            if i[0] != j:
-                clusters.append(tuple(temp_clusters))
-                temp_clusters = []
-                temp_clusters.append(i[1])
-            else:
-                temp_clusters.append(i[1])
-            j = i[0]
-        return [i for i in clusters if i]
-
     def combo(self):
         hits = []
         self.data = list(dict.fromkeys(self.data[:]))
+        self.data.sort()
         for i in range(len(self.data)):
             for j in range(len(self.data)):
                 hits.append([self.data[i],self.data[j]])
         return hits
+
+    def diff(self):
+        combos = ninjalib(self.data).combo()
+        distances = []
+        for i in combos:
+            if abs(i[0]-i[1]) > 0:
+                distances.append((abs(i[0]-i[1]),i))
+        distances.sort()
+        return distances
 
     def flatten(self):
         new_data = self.data
