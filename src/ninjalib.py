7da411fa5893e0a3c5485b84c31590b2813ad81c
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-06-06 22:47:40 -0500

    Add files via upload
    updated to be more towards data science and less just add fun math stuff

--- src/ninjalib.py
+++ src/ninjalib.py
@@ -11,21 +11,33 @@ class ninjalib:
         self.b = b
         self.c = c
 
-    def anomaly(self):
-        hits = []
+    def cluster(self):
+        combos = ninjalib(self.data).combo()
         distances = []
-        self.data.sort()
-        for key,value in enumerate(self.data):
-            if key < len(self.data) - 1:
-                distances.append(abs(self.data[key]-self.data[key+1]))
-        average = sum(distances) / len(distances)
-        for key,value in enumerate(self.data):
-            if key < len(self.data) - 1:
-                if average < abs(self.data[key+1] - self.data[key]) and average < abs(self.data[key] - self.data[key-1]):
-                    hits.append(self.data[key])
+        for i in combos:
+            distances.append((abs(i[0]-i[1]),i))
+        distances.sort()
+        j = None
+        clusters = []
+        temp_clusters = []
+        for i in distances:
+            if i[0] != j:
+                clusters.append(tuple(temp_clusters))
+                temp_clusters = []
+                temp_clusters.append(i[1])
+            else:
+                temp_clusters.append(i[1])
+            j = i[0]
+        return [i for i in clusters if i]
+
+    def combo(self):
+        hits = []
+        for i in range(len(self.data)):
+            for j in range(len(self.data)):
+                hits.append([self.data[i],self.data[j]])
         return hits
 
-    def flatten_list(self):
+    def flatten(self):
         new_data = self.data
         if self.a == 0:
             while True:
@@ -39,67 +51,5 @@ class ninjalib:
                     new_data = list(itertools.chain(*new_data))
         return new_data
 
-    def flatten_tuple(self):
-        new_data = self.data
-        if self.a == 0:
-            while True:
-                if isinstance(new_data[0],list) or isinstance(new_data[0],tuple):
-                    new_data = tuple(itertools.chain(*new_data))
-                else:
-                    break
-        else:
-            for i in range(self.aF):
-                if isinstance(new_data[0],list) or isinstance(new_data[0],tuple):
-                    new_data = tuple(itertools.chain(*new_data))
-        return new_data
-
-    def project(self):
-        try:
-            screen_x = math.floor(self.data * (self.a / self.c))
-        except ZeroDivisionError:
-            screen_x = self.data + self.a
-        try:
-            screen_y = math.floor(self.data * (self.b / self.c))
-        except ZeroDivisionError:
-            screen_y = self.data + self.b
-        return [screen_x,screen_y]
-
-    def rotate_camera(self):
-        hits = []
-        theta = math.radians(self.b)
-        center_x = []
-        center_y = []
-        center_z = []
-        for i in range(len(self.data)):
-            center_x.append(self.data[i][0])
-            center_y.append(self.data[i][1])
-            center_z.append(self.data[i][2])
-        cx = statistics.mean(center_x)
-        cy = statistics.mean(center_y)
-        cz = statistics.mean(center_z)
-        for i in range(len(self.data)):
-            x = self.data[i][0] - cx
-            y = self.data[i][1] - cy
-            z = self.data[i][2] - cz
-            if self.a == "x":
-                hits.append([round(cx+x,3),round(cy+math.cos(theta)*y-math.sin(theta)*z,3),round(cz+math.sin(theta)*y+math.cos(theta)*z,3)])
-            if self.a == "y":
-                hits.append([round(cx+math.cos(theta)*x+math.sin(theta)*z,3),round(cy+y,3),round(cz+-math.sin(theta)*x+math.cos(theta)*z,3)])
-            if self.a == "z":
-                hits.append([round(cx+math.cos(theta)*x-math.sin(theta)*y,3),round(cy+math.sin(theta)*x+math.cos(theta)*y,3),round(cz+z,3)])
-        return hits
-
     def mean(self):
         return sum(self.data) / len(self.data)
-
-    def odds(self):
-        return str(round(sum(self.data) / (sum(self.data) + sum(self.a)) * 100,3)) + "%"
-
-    def status(self):
-        if self.a == 0:
-            self.a = -1
-        handshake = binascii.unhexlify("00") + b"".join([bytes([(b := (self.a >> 7 * i) & 0x7F) | (0x80 if self.a >> 7 * (i + 1) else 0)]) for i in range(5) if (self.a >> 7 * i)]) + struct.pack(">b",len(self.data[0])) + self.data[0].encode() + struct.pack(">H", self.data[1]) + b"\x01"
-        return [struct.pack(">b",len(handshake)) + handshake, binascii.unhexlify("0100")]
-
-    def varint(self):
-        return b"".join([bytes([(b := (self.data >> 7 * i) & 0x7F) | (0x80 if self.data >> 7 * (i + 1) else 0)]) for i in range(5) if (self.data >> 7 * i)])
